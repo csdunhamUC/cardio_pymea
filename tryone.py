c60fe13e365c6596fca2f49e95ca7f0bd54e4e5a
Sapphyric <azarhn@hotmail.com>
2020-07-30 23:43:36 -0700

    Better understanding now of widget interactions.  Split intended MEA sections from file import sections, akin to the different panels in Matlab.

--- tryone.py
+++ tryone.py
@@ -7,6 +7,10 @@ import time
 import tkinter as tk
 from tkinter import filedialog
 
+root = tk.Tk()
+# Dimensions width x height, distance position from right of screen + from top of screen
+root.geometry("1600x900+900+900")
+
 # a = np.array([[1,2],[3,4]])
 # print(a)
 #
@@ -54,59 +58,54 @@ def data_import():
     data_filename = filedialog.askopenfilename(initialdir="/", title="Select file",
                                                filetypes=(("txt files", "*.txt"), ("all files", "*.*")))
     new_data = pd.read_csv(data_filename,
-                           sep='\t', lineterminator='\n', names=list(range(121)), skiprows=3,
-                           encoding='iso-8859-15', low_memory=False)
-    # new_data = pd.read_csv("/media/csdunham/0E24340D2433F675/Gimzewski Lab/Stem Cells/Data Sets/chip10002.txt",
-    #                    sep='\t', lineterminator='\n', names=list(range(121)), skiprows=3,
-    #                    encoding='iso-8859-15', low_memory=False)
+                           sep='\t', lineterminator='\n', skiprows=3, encoding='iso-8859-15', low_memory=False)
     new_data_size = np.shape(new_data)
     time_to_run = time.process_time()
     print(new_data_size)
     print(time_to_run)
+    # adding .iloc to a data frame allows to reference [row, column], where rows and columns can be ranges separated
+    # by colons
     print(new_data.iloc[2:17, 15:25])
     return new_data
 
 
-# adding .iloc to a data frame allows to reference [row, column], where rows and columns can be ranges separated
-# by colons
 # mea_data = data_import()
-
 # print(mea_data.iloc[2:17, 15:25])
 
 class ElecGUI60(tk.Frame):
     def __init__(self, parent=None):
         tk.Frame.__init__(self, parent)
         self.parent = parent
+        # The fun story about grid: columns and rows cannot be generated past the number of widgets you have (or at
+        # least I've yet to learn the way to do so, and will update if I find out how).  It's all relative geometry,
+        # where the relation is with other widgets.  If you have 3 widgets you can have 3 rows or 3 columns and
+        # organize accordingly.  If you have 2 widgets you can only have 2 rows or 2 columns.
         self.grid()
-        self.make_widgets()
+        self.file_management_widgets()
+        self.mea_array_widgets()
 
-    def make_widgets(self):
+    def file_management_widgets(self):
         self.winfo_toplevel().title("Elec GUI 60 - Prototype")
-        import_data_button = tk.Button(self, text="Import Data", width=15, height=3, bg="skyblue", command=data_import)
+        import_data_frame = tk.Frame(self, width=100, height=800, bg="white")
+        import_data_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
+        # use .bind("<Enter>", "color") or .bind("<Leave>", "color") to change mouse-over color effects.
+        # A class can be set up for the sole purpose of handling these events.
+        import_data_button = tk.Button(import_data_frame, text="Import Data", width=15, height=3, bg="skyblue",
+                                       command=data_import)
         import_data_button.grid(row=0, column=0, padx=2, pady=2)
-        save_data_button = tk.Button(self, text="Save Data", width=15, height=3, bg="lightgreen")
+        save_data_button = tk.Button(import_data_frame, text="Save Data", width=15, height=3, bg="lightgreen")
         save_data_button.grid(row=1, column=0, padx=2, pady=2)
-        # label = Entry(self, font=('Times New Roman', 20))
-        # label.pack(side="top", fill="x")
-        # gui_size = Canvas(self)
-        # gui_size.pack()
+
+    def mea_array_widgets(self):
+        mea_array_frame = tk.Frame(self, width=800, height=800, bg="white")
+        mea_array_frame.grid(row=0, column=1, padx=10, pady=10)
+        mea_array_frame.grid_propagate(False)
+        tk.Label(mea_array_frame, text="Microelectrode Array Representation").grid(row=0, column=0, padx=240, pady=2,
+                                                                                   sticky="e")
+        tk.Label(mea_array_frame, text="MEA 1").grid(row=0, column=0, padx=5, pady=2, sticky="w")
 
 
-root = tk.Tk()
-# Dimensions width x height, distance position from right of screen + from top of screen
-root.geometry("1600x900+900+900")
-# root.title('Prototyping: 7/23/2020')
 
 # Calls class to create the GUI window. *********
 elecGUI60 = ElecGUI60(root)
-
-# mea60_window = Canvas(root, width=1600, height=900)
-# mea60_window.pack()
-#
-# mea60_title = Label(root, text='60 electrode GUI prototyping')
-# mea60_title.config(font=('Times New Roman', 20))
-
-# fig.title("GUI Demo 1")
-# fig.geometry("400x400")
-
 root.mainloop()
