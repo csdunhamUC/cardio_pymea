995c82235c9fbd88c405ee3ffef1555a5b0e2672
Sapphyric <azarhn@hotmail.com>
2020-09-13 01:50:46 -0700

    9/13/2020: Resolved issues with generating subplots (at least for one of the sublots).  Distance-based find_peaks is definitely inferior to other methods and will be avoided for use.  Prominence and/or threshold, or a combination of factors, will ultimately be in play.  Next update will finish incorporating the remaining subplots.
    9/12/2020: Added conditional that checks whether raw_data was previously populated.  If so, it deletes the imported attribute assigned to the raw_data class, allowing for new values to populate raw_data and carry on through the program.
    
    9/11/2020: Resolved use of class to allow data to escape the icy grip of a function.  Next step: implement scipy's find_peaks and proper prominence settings and save pacemaker data.  Need to compare the pacemaker information calculated by this method with Matlab results.  Will be a good weekend project.

--- tryone.py
+++ tryone.py
@@ -8,6 +8,7 @@ import os
 import time
 import tkinter as tk
 from tkinter import filedialog
+from scipy.signal import find_peaks
 
 
 #
@@ -48,6 +49,10 @@ from tkinter import filedialog
 # (where PM data will go), UpstrokeVelData (where dV/dt data will go), LocalATData (where LAT data will go), and
 # CondVelData, where CV data will go.
 class ImportedData:
+    pass
+
+
+class BeatAmplitudes:
     pass
 
 
@@ -92,23 +97,54 @@ def data_import(raw_data):
     return raw_data.imported
 
 
-# Original data_import function
-# def data_import():
-#     data_filename = filedialog.askopenfilename(initialdir="/", title="Select file",
-#                                                filetypes=(("txt files", "*.txt"), ("all files", "*.*")))
-#     global raw_data
-#     print(id(raw_data))
-#     raw_data = (pd.read_csv(data_filename,
-#                            sep='\t', lineterminator='\n', skiprows=3, encoding='iso-8859-15', low_memory=False))
-#     new_data_size = np.shape(raw_data)
-#     time_to_run = time.process_time()
-#     print(new_data_size)
-#     print(time_to_run)
-#
-#     print("In function, Raw data reference: " + str(id(raw_data)))
-#
-#     return raw_data
+def determine_beats(raw_data, cm_beats):
+    print("Finding beats...\n")
+    cm_beats.x_axis = raw_data.imported.iloc[0:, 0]
+    print(cm_beats.x_axis)
+    print("\n")
+    # As of 9/12/2020 @ 10:31pm: No idea if find peaks works yet.
+    cm_beats.dist_beats = find_peaks(raw_data.imported.iloc[0:, 1], distance=1000)[0]
+    dist_beats_size = np.shape(cm_beats.dist_beats)
+    print("Shape of cm_beats.dist_beats: " + str(dist_beats_size) + ".\n")
+    print(cm_beats.dist_beats)
+    print(type(cm_beats.dist_beats))
 
+    cm_beats.prom_beats = find_peaks(raw_data.imported.iloc[0:, 1], prominence=100)[0]
+    #print(cm_beats.prom_beats)
+
+    cm_beats.width_beats = find_peaks(raw_data.imported.iloc[0:, 1], width=5)[0]
+    #print(cm_beats.width_beats)
+
+    cm_beats.thresh_beats = find_peaks(raw_data.imported.iloc[0:, 1], threshold=100)[0]
+    #print(cm_beats.thresh_beats)
+
+    time_to_run = time.process_time()
+    print("Finished.")
+    print(time_to_run)
+    print("Plotting...")
+    graph_peaks(raw_data, cm_beats)
+
+def graph_peaks(raw_data, cm_beats):
+
+    #     cm_beats.axis1 = cm_beats.comp_plot.add_subplot(221)
+    #     cm_beats.axis2 = cm_beats.comp_plot.add_subplot(222)
+    #     cm_beats.axis3 = cm_beats.comp_plot.add_subplot(223)
+    #     cm_beats.axis4 = cm_beats.comp_plot.add_subplot(224)
+    # I need the amplitudes that correspond to the given indices
+    # I need the amplitudes from raw_data.imported.iloc, column 2 that correspond to the given indices
+    # How do I achieve this?
+    place_holder = raw_data.imported.iloc[0:, 1]
+
+    cm_beats.axis1.plot(cm_beats.dist_beats, raw_data.imported.iloc[0:, 1][cm_beats.dist_beats], "xr")
+    cm_beats.axis1.plot(cm_beats.x_axis, raw_data.imported.iloc[0:, 1])
+    cm_beats.axis1.legend(['distance'])
+
+    # cm_beats.axis1.plot(cm_beats.x_axis, cm_beats.dist_beats, "xr")
+    # cm_beats.axis2.plot(cm_beats.x_axis, cm_beats.prom_beats, "ob")
+    # cm_beats.axis3.plot(cm_beats.x_axis, cm_beats.width_beats, "vg")
+    # cm_beats.axis4.plot(cm_beats.x_axis, cm_beats.thresh_beats, "xk")
+    cm_beats.comp_plot.canvas.draw()
+    print("Did it work?")
 
 def data_print(raw_data):
     # adding .iloc to a data frame allows to reference [row, column], where rows and columns can be ranges separated
@@ -116,12 +152,6 @@ def data_print(raw_data):
     print(id(raw_data.imported))
     print(raw_data.imported.iloc[15:27, 0:15])
 
-
-# def data_print(raw_data):
-#     # adding .iloc to a data frame allows to reference [row, column], where rows and columns can be ranges separated
-#     # by colons
-#     print(id(raw_data))
-#     print(raw_data.iloc[15:27, 0:15])
 
 def graph_heatmap(vegetables, farmers, harvest, heat_map, axis1):
     # imshow() is the key heatmap function here.
@@ -146,17 +176,17 @@ def graph_heatmap(vegetables, farmers, harvest, heat_map, axis1):
 
 
 class ElecGUI60(tk.Frame):
-    def __init__(self, master, raw_data):
+    def __init__(self, master, raw_data, cm_beats):
         tk.Frame.__init__(self, master)
         # The fun story about grid: columns and rows cannot be generated past the number of widgets you have (or at
         # least I've yet to learn the way to do so, and will update if I find out how).  It's all relative geometry,
         # where the relation is with other widgets.  If you have 3 widgets you can have 3 rows or 3 columns and
         # organize accordingly.  If you have 2 widgets you can only have 2 rows or 2 columns.
         self.grid()
-        self.file_management_widgets(raw_data)
-        self.mea_array_widgets()
+        self.file_management_widgets(raw_data, cm_beats)
+        self.mea_array_widgets(cm_beats)
 
-    def file_management_widgets(self, raw_data):
+    def file_management_widgets(self, raw_data, cm_beats):
         self.winfo_toplevel().title("Elec GUI 60 - Prototype")
         file_operations = tk.Frame(self, width=100, height=800, bg="white")
         file_operations.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
@@ -176,20 +206,33 @@ class ElecGUI60(tk.Frame):
                                       command=lambda: data_print(raw_data))
         print_data_button.grid(row=2, column=0, padx=2, pady=2)
 
+        calc_peaks_button = tk.Button(file_operations, text="Find Peaks", width=15, height=3, bg="orange",
+                                      command=lambda: determine_beats(raw_data, cm_beats))
+        calc_peaks_button.grid(row=3, column=0, padx=2, pady=2)
+
         # to generate heatmap; currently only generates for Matplotlib demo data.
         graph_heatmap_button = tk.Button(file_operations, text="Graph Heatmap", width=15, height=3, bg="red",
                                          command=lambda: graph_heatmap(vegetables, farmers, harvest, heat_map, axis1))
-        graph_heatmap_button.grid(row=3, column=0, padx=2, pady=2)
+        graph_heatmap_button.grid(row=4, column=0, padx=2, pady=2)
 
         # Eventually this function will house the widgets necessary to display heat maps generated for MEAs.
 
-    def mea_array_widgets(self):
+    def mea_array_widgets(self, cm_beats):
         mea_array_frame = tk.Frame(self, width=1200, height=800, bg="white")
         mea_array_frame.grid(row=0, column=1, padx=10, pady=10)
         mea_array_frame.grid_propagate(False)
 
+        beat_detect_frame = tk.Frame(self, width=1200, height=800, bg="white")
+        beat_detect_frame.grid(row=0, column=2, padx=10, pady=10)
+        beat_detect_frame.grid_propagate(False)
+
         gen_figure = FigureCanvasTkAgg(heat_map, mea_array_frame)
         gen_figure.get_tk_widget().grid(row=0, column=0, padx=10, pady=10)
+
+        gen_beats = FigureCanvasTkAgg(cm_beats.comp_plot, beat_detect_frame)
+        gen_beats.get_tk_widget().grid(row=0, column=0, padx=10, pady=10)
+
+        #gen_beat_figure = FigureCanvasTkAgg
 
         # tk.Label(mea_array_frame, text="Microelectrode Array Representation").grid(row=0, column=0, padx=240, pady=2,
         #                                                                            sticky="e")
@@ -223,16 +266,23 @@ def main():
     axis1 = heat_map.add_subplot(111)
 
     raw_data = ImportedData()
+    cm_beats = BeatAmplitudes()
+    cm_beats.comp_plot = plt.Figure(figsize=(10, 7), dpi=120)
+    cm_beats.comp_plot.suptitle("Comparisons of find_peaks methodologies")
+    cm_beats.axis1 = cm_beats.comp_plot.add_subplot(221)
+    cm_beats.axis2 = cm_beats.comp_plot.add_subplot(222)
+    cm_beats.axis3 = cm_beats.comp_plot.add_subplot(223)
+    cm_beats.axis4 = cm_beats.comp_plot.add_subplot(224)
 
     # import_raw_data = pd.DataFrame()
     # print("Import data memory reference: " + str(id(import_raw_data)))
 
     root = tk.Tk()
     # Dimensions width x height, distance position from right of screen + from top of screen
-    root.geometry("1600x900+900+900")
+    root.geometry("2700x900+900+900")
 
     # Calls class to create the GUI window. *********
-    elecGUI60 = ElecGUI60(root, raw_data)
+    elecGUI60 = ElecGUI60(root, raw_data, cm_beats)
     root.mainloop()
 
 
