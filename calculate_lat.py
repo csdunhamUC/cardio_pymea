3f7a79766354e636d962d258a4dbd0c8dcb3fb11
Sapphyric <csdunham@chem.ucla.edu>
2021-01-21 23:58:21 -0800

    Updated to handle 1000Hz or 10,000Hz SF.

--- calculate_lat.py
+++ calculate_lat.py
@@ -25,19 +25,29 @@ def calculate_lat(analysisGUI, cm_beats, local_act_time, heat_map, input_param, 
         cm_beats_dist_beats_as_array = cm_beats.dist_beats.to_numpy()
         cm_beats_y_axis_as_array = cm_beats.y_axis.to_numpy()
         per_electrode_mode_of_beats = np.zeros(num_of_electrodes)
+        sample_frequency = input_param.sample_frequency
 
         for electrode in range(num_of_electrodes):
             per_electrode_mode_of_beats[electrode] = len(cm_beats.dist_beats.iloc[0:, electrode].dropna())
 
-        local_at = lat_calc_for_numba(mode_of_beats, num_of_electrodes, cm_beats_dist_beats_as_array,
-            cm_beats_y_axis_as_array, per_electrode_mode_of_beats)
+        local_at = lat_calc_for_numba(mode_of_beats, num_of_electrodes, 
+            cm_beats_dist_beats_as_array, cm_beats_y_axis_as_array, 
+            per_electrode_mode_of_beats, sample_frequency)
 
         local_act_time.final_dist_beat_count = []
         for beat in range(int(cm_beats.beat_count_dist_mode[0])):
             local_act_time.final_dist_beat_count.append('Beat ' + str(beat + 1))
 
+        # Consolidate raw data into local_act_time.
         local_act_time.param_dist_raw = pd.DataFrame(local_at, index=local_act_time.final_dist_beat_count).T
-        local_act_time.param_dist_normalized = local_act_time.param_dist_raw.sub(local_act_time.param_dist_raw.min(axis=0), axis=1)
+        
+        # Normalize LAT.
+        if 1000 / input_param.sample_frequency == 1.0:
+            local_act_time.param_dist_normalized = local_act_time.param_dist_raw.sub(
+                local_act_time.param_dist_raw.min(axis=0), axis=1)
+        elif 1000 / input_param.sample_frequency == 0.1:
+            local_act_time.param_dist_normalized = local_act_time.param_dist_raw.sub(
+                local_act_time.param_dist_raw.min(axis=0), axis=1).div(10)
 
         # Find maximum time lag, LAT version (interval)
         local_act_time.param_dist_normalized_max = local_act_time.param_dist_normalized.max().max()
@@ -68,59 +78,90 @@ def calculate_lat(analysisGUI, cm_beats, local_act_time, heat_map, input_param, 
 
 
 @njit
-def lat_calc_for_numba(mode_of_beats, num_of_electrodes, cm_beats_dist_beats_as_array, cm_beats_y_axis_as_array, per_electrode_mode_of_beats):
-    temp_slope = np.zeros((num_of_electrodes, 5))
-    temp_index = np.zeros((num_of_electrodes, 5))
+def lat_calc_for_numba(mode_of_beats, num_of_electrodes, 
+cm_beats_dist_beats_as_array, cm_beats_y_axis_as_array, 
+per_electrode_mode_of_beats, sample_frequency):
     temp_local_at = np.zeros(num_of_electrodes)
-    x_values = np.zeros((2, 5))
-    y_values = np.zeros((2, 5))
     local_at = np.zeros((mode_of_beats, num_of_electrodes))
 
-    for beat in range(mode_of_beats):
-        for electrode in range(num_of_electrodes):
-            if mode_of_beats == per_electrode_mode_of_beats[electrode]:
-                x_2_1 = cm_beats_dist_beats_as_array[beat, electrode]
-                x_2_2 = x_2_1 + 1
-                x_2_3 = x_2_2 + 1
-                x_2_4 = x_2_3 + 1
-                x_2_5 = x_2_4 + 1
+    
+    if 1000 / sample_frequency == 1.0:
+        end_point = 5
+        temp_slope = np.zeros((num_of_electrodes, end_point))
+        temp_index = np.zeros((num_of_electrodes, end_point))
+        x_values = np.zeros((2, end_point))
+        y_values = np.zeros((2, end_point))
+        for beat in range(mode_of_beats):
+            for electrode in range(num_of_electrodes):
+                if mode_of_beats == per_electrode_mode_of_beats[electrode]:
+                    x_values[0, 0] = cm_beats_dist_beats_as_array[beat, electrode]
+                    x_values[1, 0] = x_values[0, 0] + 1
+                    y_values[0, 0] = cm_beats_y_axis_as_array[
+                        int(x_values[0, 0]), electrode]
+                    y_values[1, 0] = cm_beats_y_axis_as_array[
+                        int(x_values[1, 0]), electrode]
+                    for index in range(1, end_point):
+                        x_values[0, index] = x_values[0, 0] + index
+                        x_values[1, index] = x_values[1, 0] + index
+                        
+                        y_values[0, index] = cm_beats_y_axis_as_array[
+                            int(x_values[0, index]), electrode]
+                        y_values[1, index] = cm_beats_y_axis_as_array[
+                            int(x_values[1, index]), electrode]
 
-                x_1_1 = x_2_1 + 1
-                x_1_2 = x_2_2 + 1
-                x_1_3 = x_2_3 + 1
-                x_1_4 = x_2_4 + 1
-                x_1_5 = x_2_5 + 1
+                    y_diff = y_values[0, :] - y_values[1, :]
+                    x_diff = x_values[0, :] - x_values[1, :]
 
-                y_2_1 = cm_beats_y_axis_as_array[int(x_2_1), electrode]
-                y_2_2 = cm_beats_y_axis_as_array[int(x_2_2), electrode]
-                y_2_3 = cm_beats_y_axis_as_array[int(x_2_3), electrode]
-                y_2_4 = cm_beats_y_axis_as_array[int(x_2_4), electrode]
-                y_2_5 = cm_beats_y_axis_as_array[int(x_2_5), electrode]
+                    calc_slope = np.divide(y_diff, x_diff)
 
-                y_1_1 = cm_beats_y_axis_as_array[int(x_1_1), electrode]
-                y_1_2 = cm_beats_y_axis_as_array[int(x_1_2), electrode]
-                y_1_3 = cm_beats_y_axis_as_array[int(x_1_3), electrode]
-                y_1_4 = cm_beats_y_axis_as_array[int(x_1_4), electrode]
-                y_1_5 = cm_beats_y_axis_as_array[int(x_1_5), electrode]
+                    temp_index[electrode] = x_values[0]
+                    temp_slope[electrode] = calc_slope
+                else:
+                    temp_index[electrode] = [np.nan] * end_point
+                    temp_slope[electrode] = [np.nan] * end_point
+                
+                temp_local_at[electrode] = temp_index[electrode, np.argmin(temp_slope[electrode])]
+            
+            local_at[beat] = temp_local_at
+    
+    elif 1000 / sample_frequency == 0.1:
+        end_point = 50
+        temp_slope = np.zeros((num_of_electrodes, end_point))
+        temp_index = np.zeros((num_of_electrodes, end_point))
+        x_values = np.zeros((2, end_point))
+        y_values = np.zeros((2, end_point))
+        for beat in range(mode_of_beats):
+            for electrode in range(num_of_electrodes):
+                if mode_of_beats == per_electrode_mode_of_beats[electrode]:
+                    x_values[0, 0] = cm_beats_dist_beats_as_array[beat, electrode]
+                    x_values[1, 0] = x_values[0, 0] + 1
+                    y_values[0, 0] = cm_beats_y_axis_as_array[
+                        int(x_values[0, 0]), electrode]
+                    y_values[1, 0] = cm_beats_y_axis_as_array[
+                        int(x_values[1, 0]), electrode]
+                    for index in range(1, end_point):
+                        x_values[0, index] = x_values[0, 0] + index
+                        x_values[1, index] = x_values[1, 0] + index
+                        
+                        y_values[0, index] = cm_beats_y_axis_as_array[
+                            int(x_values[0, index]), electrode]
+                        y_values[1, index] = cm_beats_y_axis_as_array[
+                            int(x_values[1, index]), electrode]
+                    
+                    y_diff = y_values[0, :] - y_values[1, :]
+                    x_diff = x_values[0, :] - x_values[1, :]
 
-                x_values[0, :] = [x_2_1, x_2_2, x_2_3, x_2_4, x_2_5]
-                x_values[1, :] = [x_1_1, x_1_2, x_1_3, x_1_4, x_1_5]
-                y_values[0, :] = [y_2_1, y_2_2, y_2_3, y_2_4, y_2_5]
-                y_values[1, :] = [y_1_1, y_1_2, y_1_3, y_1_4, y_1_5]
+                    calc_slope = np.divide(y_diff, x_diff)
 
-                y_diff = y_values[0, :] - y_values[1, :]
-                x_diff = x_values[0, :] - x_values[1, :]
-
-                calc_slope = np.divide(y_diff, x_diff)
-
-                temp_index[electrode] = x_values[0]
-                temp_slope[electrode] = calc_slope
-            else:
-                temp_index[electrode] = [np.nan, np.nan, np.nan, np.nan, np.nan]
-                temp_slope[electrode] = [np.nan, np.nan, np.nan, np.nan, np.nan]
-            temp_local_at[electrode] = temp_index[electrode, np.argmin(temp_slope[electrode])]
-
-        local_at[beat] = temp_local_at
+                    temp_index[electrode] = x_values[0]
+                    temp_slope[electrode] = calc_slope
+                else:
+                    temp_index[electrode] = [np.nan] * end_point
+                    temp_slope[electrode] = [np.nan] * end_point
+                
+                temp_local_at[electrode] = temp_index[electrode, np.argmin(temp_slope[electrode])]
+            
+            local_at[beat] = temp_local_at
 
     return local_at
 
