dec25d2365e1270be3146a77cbbffdbf4071a108
Sapphyric <csdunham@chem.ucla.edu>
2021-01-25 01:23:36 -0800

    Working to implement various PSD/other features.

--- psd_plotting.py
+++ psd_plotting.py
@@ -40,24 +40,65 @@ import time
 
 def psd_plotting(analysisGUI, cm_beats, pace_maker, upstroke_vel, 
 local_act_time, conduction_vel, input_param, psd_data):
+    # Update slider max.
+    analysisGUI.psd_electrode_select.configure(to=int(cm_beats.beat_count_dist_mode[0]))
+    # Get slider value.
+    input_param.psd_plot_slider = (analysisGUI.psd_electrode_select.get() - 1)
     
+    # Update entry boxes for PSD Plotting window.
+    analysisGUI.psd_start_beat_value['values'] = pace_maker.final_dist_beat_count
+    analysisGUI.psd_end_beat_value['values'] = pace_maker.final_dist_beat_count
+
     plot_log_vs_log(analysisGUI, cm_beats, pace_maker, upstroke_vel, 
+        local_act_time, conduction_vel, input_param, psd_data)
+    plot_psd_welch(analysisGUI, cm_beats, pace_maker, upstroke_vel, 
         local_act_time, conduction_vel, input_param, psd_data)
 
 
 def plot_log_vs_log(analysisGUI, cm_beats, pace_maker, upstroke_vel, 
 local_act_time, conduction_vel, input_param, psd_data):
-    start_beat = analysisGUI.psd_start_beat_value.cget()
-    end_beat = analysisGUI.psd_end_beat_value.cget()
+    start_beat = analysisGUI.psd_start_beat_value.get()
+    print(start_beat)
+    end_beat = analysisGUI.psd_end_beat_value.get()
+    print(end_beat)
 
-    psd_data.loglog_during_ax.loglog(local_act_time.distance_from)
+    # Get slider value.
+    input_param.psd_plot_slider = (analysisGUI.psd_electrode_select.get() - 1)
+
+    psd_data.loglog_during_ax.cla()
+
+    psd_data.loglog_during_ax.loglog(local_act_time.distance_from_min[
+        pace_maker.final_dist_beat_count[input_param.psd_plot_slider]],
+        conduction_vel.param_dist_raw[pace_maker.final_dist_beat_count[
+            input_param.psd_plot_slider]], '.', base=10)
+    
+    print(pace_maker.final_dist_beat_count[input_param.psd_plot_slider])
+
+    psd_data.psd_plots.tight_layout()
+    psd_data.psd_plots.canvas.draw()
 
 
 def plot_psd_welch(analysisGUI, cm_beats, pace_maker, upstroke_vel, 
 local_act_time, conduction_vel, input_param, psd_data):
-    print()
-    x = np.ndarray((2,3))
-    freq, Pxx = signal.periodogram(x, fs=1.0, window='hann')
+    # psd_data.psd_during_ax.cla()
+    # x = np.ndarray((2,3))
+    freq, Pxx = signal.welch(conduction_vel.param_dist_raw[pace_maker.final_dist_beat_count[
+            input_param.psd_plot_slider]].dropna(), fs=1.0, window='hann')
+    # print(conduction_vel.param_dist_raw.drop(
+    #     columns=['Electrode', 'X', 'Y']).dropna().values.flatten())
+    # print(len(conduction_vel.param_dist_raw.drop(
+    #     columns=['Electrode', 'X', 'Y']).dropna().values.flatten()))
+
+    # freq, Pxx = signal.welch(conduction_vel.param_dist_raw.drop(
+    #     columns=['Electrode', 'X', 'Y']).dropna().values.flatten(), fs=1.0, window='hann')
     # x must be a 1d array or series, fs = sampling frequency
-    freq, Pxx = signal.welch(x, fs=1.0)
+    # freq, Pxx = signal.welch(x, fs=1.0)
+    # print(freq)
+    # print(Pxx_period)
+
+    psd_data.psd_during_ax.loglog(freq, Pxx, label=pace_maker.final_dist_beat_count[
+            input_param.psd_plot_slider])
+    psd_data.psd_during_ax.legend(loc="lower left", ncol=3)
+    psd_data.psd_plots.tight_layout()
+    psd_data.psd_plots.canvas.draw()
     # Welch may be the more robust method to use.
