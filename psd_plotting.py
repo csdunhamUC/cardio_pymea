9827743c61e9485ec4d97b13b5c0995fb8452bf0
Sapphyric <csdunham@chem.ucla.edu>
2021-01-26 02:14:31 -0800

    Implemented toggle well for PSD.  Need to repeat for log vs log, as well as add remaining params.

--- psd_plotting.py
+++ psd_plotting.py
@@ -3,7 +3,8 @@
 # Gimzewski Lab @ UCLA, Department of Chemistry & Biochem
 # Original work
 
-# PSD and log-log plotting functions for operation using mea-analysis program.
+# Log vs Log and PSD (also Log vs Log) plotting functions for operation using 
+# mea-analysis program.
 
 import numpy as np
 import scipy as sp
@@ -23,19 +24,11 @@ import time
     psd_data.psd_during_ax = psd_data.psd_plots.add_subplot(326)
     cm_beats.x_axis
     cm_beats.y_axis
-    pace_maker.param_dist_normalized
-    local_act_time.param_dist_normalized
-    upstroke_vel.param_dist_normalized
-    conduction_vel.param_dist_raw
     input_param.sample_frequency
-    analysisGUI.psd_start_beat.cget()
-    analysisGUI.psd_end_beat.cget()
-    analysisGUI.psd_start_beat_value.cget()
-    analysisGUI.psd_end_beat_value.cget()
     input_param.psd_plot_slider = int(
         analysisGUI.psd_electrode_select.get()) - 1
     local_act_time.distance_from_min[pace_maker.final_dist_beat_count[
-            input_param.psd_plot_slider]]
+        input_param.psd_plot_slider]]
 """
 
 def psd_plotting(analysisGUI, cm_beats, electrode_config, pace_maker, upstroke_vel, 
@@ -54,28 +47,41 @@ local_act_time, conduction_vel, input_param, psd_data):
     end_beat = analysisGUI.psd_end_beat_value.get()
     elec_choice = analysisGUI.psd_electrode_choice.get()
 
+    param_choices = {"Orig. Signal": cm_beats.y_axis, 
+        "Cond. Vel.": conduction_vel.param_dist_raw,
+        "Up. Vel.": upstroke_vel.param_dist_normalized, 
+        "Pacemaker": pace_maker.param_dist_normalized, 
+        "Local AT": local_act_time.param_dist_normalized}
+
     if len(psd_data.loglog_during_ax.lines) >= 12:
         psd_data.psd_during_ax.cla()
         psd_data.loglog_during_ax.cla()
 
     plot_log_vs_log(analysisGUI, cm_beats, pace_maker, upstroke_vel, 
         local_act_time, conduction_vel, input_param, psd_data, start_beat, 
-        end_beat, elec_choice)
+        end_beat, elec_choice, param_choices)
+    
     plot_psd_welch(analysisGUI, cm_beats, pace_maker, upstroke_vel, 
         local_act_time, conduction_vel, input_param, psd_data, start_beat, 
-        end_beat, elec_choice)
+        end_beat, elec_choice, param_choices, electrode_config)
 
 
 def plot_log_vs_log(analysisGUI, cm_beats, pace_maker, upstroke_vel, 
 local_act_time, conduction_vel, input_param, psd_data, start_beat, end_beat,
-elec_choice):
+elec_choice, param_choices):
 
+    # Only here for testing, will remove later.
     print(pace_maker.param_dist_raw.loc[elec_choice, start_beat])
     print(pace_maker.param_dist_raw.loc[elec_choice, end_beat])
 
     # Get slider value.
     input_param.psd_plot_slider = (analysisGUI.psd_electrode_select.get() - 1)
 
+    check_param = analysisGUI.psd_param_choice.get()
+    if check_param == "Orig. Signal":
+        print()
+
+    # Produce log vs log plot of parameter vs distance.
     psd_data.loglog_during_ax.loglog(local_act_time.distance_from_min[
         pace_maker.final_dist_beat_count[input_param.psd_plot_slider]],
         conduction_vel.param_dist_raw[pace_maker.final_dist_beat_count[
@@ -87,20 +93,47 @@ elec_choice):
 
     print(pace_maker.final_dist_beat_count[input_param.psd_plot_slider])
 
-    psd_data.psd_plots.tight_layout()
-    psd_data.psd_plots.canvas.draw()
-
 
 def plot_psd_welch(analysisGUI, cm_beats, pace_maker, upstroke_vel, 
 local_act_time, conduction_vel, input_param, psd_data, start_beat, end_beat,
-elec_choice):
+elec_choice, param_choices, electrode_config):
     # x = np.ndarray((2,3))
-    freq, Pxx = signal.welch(conduction_vel.param_dist_raw[pace_maker.final_dist_beat_count[
-            input_param.psd_plot_slider]].dropna(), fs=1.0, window='hann')
+
     # print(conduction_vel.param_dist_raw.drop(
     #     columns=['Electrode', 'X', 'Y']).dropna().values.flatten())
     # print(len(conduction_vel.param_dist_raw.drop(
     #     columns=['Electrode', 'X', 'Y']).dropna().values.flatten()))
+
+    check_param = analysisGUI.psd_param_choice.get()
+    if check_param == "Orig. Signal":
+        param_plotted = param_choices.get(check_param)
+        start_index = int(pace_maker.param_dist_raw.loc[elec_choice, start_beat])
+        end_index = int(pace_maker.param_dist_raw.loc[elec_choice, end_beat])
+        elec_index = electrode_config.electrode_names.index(elec_choice)
+        # print("Electrode: " + str(elec_choice) + "\n" + "Index: " + 
+        #     str(electrode_config.electrode_names.index(elec_choice)))
+        freq, Pxx = signal.welch(param_plotted.iloc[start_index:end_index, 
+            elec_index], fs=1.0, window='hann')
+        
+        psd_data.psd_during_ax.loglog(freq, Pxx, 
+            label=elec_choice)
+        psd_data.psd_during_ax.set(title="Welch PSD of Signal", 
+            xlabel="Log of Frequency (Hz)", ylabel="Log of PSD of Signal")
+        psd_data.psd_during_ax.legend(loc="lower left", ncol=6)
+    
+    elif check_param == "Cond. Vel.":
+        param_plotted = param_choices.get(check_param)
+        freq, Pxx = signal.welch(param_plotted[pace_maker.final_dist_beat_count[
+            input_param.psd_plot_slider]].dropna(), fs=1.0, window='hann')
+        
+        psd_data.psd_during_ax.loglog(freq, Pxx, 
+            label=pace_maker.final_dist_beat_count[input_param.psd_plot_slider])
+        psd_data.psd_during_ax.set(title="Welch PSD of CV", 
+            xlabel="Log of Frequency (Hz)", ylabel="Log of PSD of CV")
+        psd_data.psd_during_ax.legend(loc="lower left", ncol=6)
+    
+    psd_data.psd_plots.tight_layout()
+    psd_data.psd_plots.canvas.draw()
 
     # freq, Pxx = signal.welch(conduction_vel.param_dist_raw.drop(
     #     columns=['Electrode', 'X', 'Y']).dropna().values.flatten(), fs=1.0, window='hann')
@@ -108,12 +141,3 @@ elec_choice):
     # freq, Pxx = signal.welch(x, fs=1.0)
     # print(freq)
     # print(Pxx_period)
-
-    psd_data.psd_during_ax.loglog(freq, Pxx, 
-        label=pace_maker.final_dist_beat_count[input_param.psd_plot_slider])
-    psd_data.psd_during_ax.set(title="Welch PSD of CV", 
-        xlabel="Log of Frequency (Hz)", ylabel="Log of PSD of CV")
-    psd_data.psd_during_ax.legend(loc="lower left", ncol=6)
-    psd_data.psd_plots.tight_layout()
-    psd_data.psd_plots.canvas.draw()
-    # Welch may be the more robust method to use.
