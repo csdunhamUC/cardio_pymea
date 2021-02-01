f2b1b8ebf87f46d4a3a58f9559f69161fb43a3bf
Sapphyric <csdunham@chem.ucla.edu>
2021-01-31 17:46:41 -0800

    Vector field "works".  Something is still off with data inputs.

--- cv_quiver.py
+++ cv_quiver.py
@@ -5,16 +5,31 @@
 
 import numpy as np
 import pandas as pd
-import scipy as sp
-from scipy.optimize import curve_fit
-# import seaborn as sns
+import seaborn as sns
 from matplotlib import pyplot as plt
 
 def cv_quiver_plot(analysisGUI, input_param, local_act_time, conduction_vel):
+    # X and Y electrode coordinates
+    conduction_vel.quiver_plot_axis.cla()
+
     x_arrow = conduction_vel.vector_x_comp['X'].values
     y_arrow = conduction_vel.vector_y_comp['Y'].values
+
+    input_param.cv_vect_beat_choice = int(
+        analysisGUI.cv_vector_beat_select.get()) - 1
+
+    # Generate quiver plot using x, y arrow coordinates and magnitudes from
+    # calculate_cv function.
     conduction_vel.quiver_plot_axis.quiver(x_arrow, y_arrow,
         conduction_vel.vector_x_comp[local_act_time.final_dist_beat_count[
-            input_param.cv_solo_beat_choice]], 
+            input_param.cv_vect_beat_choice]], 
         conduction_vel.vector_y_comp[local_act_time.final_dist_beat_count[
-            input_param.cv_solo_beat_choice]])
+            input_param.cv_vect_beat_choice]])
+    
+    # Invert y-axis
+    conduction_vel.quiver_plot_axis.set_ylim(
+        conduction_vel.quiver_plot_axis.get_ylim()[::-1])
+
+    # Draw plot.
+    conduction_vel.quiver_plot.tight_layout()
+    conduction_vel.quiver_plot.canvas.draw()
