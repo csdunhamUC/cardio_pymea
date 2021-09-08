fb0918be11ce8709700e1391dc90e589fa33bae0
Chris Dunham <azarhn@hotmail.com>
2021-09-08 00:07:05 -0700

    Working. Needs enhancement for robustness e.g. dispersed multi-min PMs.

--- detect_transloc.py
+++ detect_transloc.py
@@ -21,13 +21,19 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
 
     # "Size" of event, or length in beats the pacemaker is at some electrode
     event_length = 0
+    # Store event lengths in list for however many events there are.
+    event_length_list = []
     # Threshold distance in micrometers (microns)
     thresh = 800
-    # Algorithm
+    # List to store pacemaker electrodes for checking against previous beat
+    pm_electrode = []
+    
+    # Algorithm begins.
     """
+    Pseudocode
     What needs to be done for this algorithm to be successful:
 
-    Locate PM electrode.
+    Identify PM electrode.
     Monitor PM electrode through each beat
     Stop when PM electrode is no longer the PM electrode
         Check the other electrodes
@@ -42,11 +48,10 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
                 Continue search for an event
     
     """
-    for beat in pm_only_all_beats.columns[3:13]:
+    for num, beat in enumerate(pm_only_all_beats.columns[3:]):
         current_pm_idx = np.where(pm_only_all_beats[beat] == 0)
         num_pm = np.shape(current_pm_idx)[1]
         
-        # current_pm = []
         # Decide between pacemaker electrodes in the event there are multiple
         # for a given beat
         if num_pm > 1:
@@ -54,22 +59,86 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
             for idx in current_pm_idx[0]:
                 temp_pm.append(pm_elecs[idx])
 
+            # When multiple minima exist, confirm their distances are under 
+            # the threshold.
             current_pm = distance_calc(
                 pm_only_all_beats[["Electrode", "X", "Y", beat]], 
                 temp_pm, thresh=thresh,
                 calc_mode="multi_min")
-            print(current_pm)
+            # print(f"Inside multi-if: {current_pm}")
+            if current_pm == None:
+                print("Terminating due to widely dispersed pacemakers in beat.\n")
+                print("Consider re-evaluating your data.")
+                break
+            pm_electrode.append(current_pm)
+            
+            # Check whether the current pacemaker electrode is the same
+            # electrode as in the previous beat
+            if (num != 0):
+                # print(num)
+                if (pm_electrode[num] != pm_electrode[num-1]):
+                    old_elec = pm_electrode[num-1]
+                    new_elec = pm_electrode[num]
+                    print(f"Electrode changed from {old_elec} to {new_elec}.")
+
+                    pm_old_new = [old_elec, new_elec]
+
+                    # Boolean check whether electrode distance exceeds
+                    # threshold
+                    check_thresh = distance_calc(
+                        pm_only_all_beats[["Electrode", "X", "Y", beat]], 
+                        pm_old_new, thresh=thresh,
+                        calc_mode="new_min")
+
+                    if check_thresh == True:
+                        event_length_list.append(event_length)
+                        event_length = 1
+                    elif check_thresh == False:
+                        event_length += 1
+                else:
+                    event_length += 1
+            else:
+                event_length += 1
 
         else:
-            pm_idx = pm_elecs[current_pm_idx[0][0]]
-            print(pm_idx)
-            # current_pm = current_pm_idx[0][num]
-            # print(current_pm)
-            # print(current_pm_idx[0][0])
-            # print(current_pm_idx[0][1])
-        # When multiple minima exist, confirm their distances are under thresh
+            current_pm = pm_elecs[current_pm_idx[0][0]]
+            # print(f"Inside else: {current_pm}")
+            pm_electrode.append(current_pm)
+            
+            # Check whether the current pacemaker electrode is the same
+            # electrode as in the previous beat
+            if (num != 0):
+                # print(num)
+                if (pm_electrode[num] != pm_electrode[num-1]):
+                    old_elec = pm_electrode[num-1]
+                    new_elec = pm_electrode[num]
+                    print(f"Electrode changed from {old_elec} to {new_elec}.")
+
+                    pm_old_new = [old_elec, new_elec]
+
+                    # Boolean check whether electrode distance exceeds
+                    # threshold
+                    check_thresh = distance_calc(
+                        pm_only_all_beats[["Electrode", "X", "Y", beat]], 
+                        pm_old_new, thresh=thresh,
+                        calc_mode="new_min")
+
+                    if check_thresh == True:
+                        event_length_list.append(event_length)
+                        event_length = 1
+                    elif check_thresh == False:
+                        event_length += 1
+                else:
+                    event_length += 1
+            else:
+                event_length += 1
+
+        
         # current_pm = pm_only_all_beats.loc[pm_only_all_beats[beat] == 0]
         # print(current_pm)
+    event_length_list.append(event_length)
+    print(pm_electrode)
+    print(event_length_list)
 
 
 def distance_calc(pacemaker_only_df: DataFrame, pacemaker_elec: List, 
@@ -81,7 +150,6 @@ thresh: int, calc_mode=""):
         calc_df = pacemaker_only_df[["X", "Y"]]
         for fixed_elec in pacemaker_elec:
             pm_fixed = calc_df.loc[fixed_elec]
-            # print(pm_fixed)
         
             for elec in pacemaker_elec:
                 x1 = calc_df.loc[elec, "X"]
@@ -90,32 +158,28 @@ thresh: int, calc_mode=""):
                 y2 = pm_fixed["Y"]
                 dist = np.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
                 temp_dists.append(dist)
-        # print(temp_dists)
         
         if all(dist < thresh for dist in temp_dists):
             return pacemaker_elec[0]
         else:
-            print("Problem: pacemakers for given beat are far apart.")
-            print("Terminating. Consider re-evaluating your data.")
+            print("Problem: pacemakers for this beat are far apart.")
+            return None
 
-
-    if calc_mode == "full":
-        elecs = pacemaker_only_df.index
-        print()
-    
-    # for elec_fixed in elecs:
-    #     pm_fixed_loc = elecs[elec_fixed]
-    #     temp_dists = []
-
-    #     for elec in elecs:
-    #         x1 = elecs[elec][0]
-    #         x2 = pm_fixed_loc[0]
-    #         y1 = elecs[elec][1]
-    #         y2 = pm_fixed_loc[1]
-
-    #         dist = np.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
-    #         temp_dists.append(dist)
-    
-    # distances.append(temp_dists)
-
-    return distances
+    if calc_mode == "new_min":
+        temp_dists = []
+        calc_df = pacemaker_only_df[["X", "Y"]]
+        for fixed_elec in pacemaker_elec:
+            pm_fixed = calc_df.loc[fixed_elec]
+        
+            for elec in pacemaker_elec:
+                x1 = calc_df.loc[elec, "X"]
+                y1 = calc_df.loc[elec, "Y"]
+                x2 = pm_fixed["X"]
+                y2 = pm_fixed["Y"]
+                dist = np.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
+                temp_dists.append(dist)
+        
+        if any(dist > thresh for dist in temp_dists):
+            return True
+        else:
+            return False
