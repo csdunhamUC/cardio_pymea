0379cb7e106612425c3bc7acec4e484eee7638c1
Chris Dunham <azarhn@hotmail.com>
2021-09-13 18:03:13 -0700

    Reconciled multi-min PMs that are far apart.

--- detect_transloc.py
+++ detect_transloc.py
@@ -29,10 +29,20 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
         min_elecs_idx = np.where(min_pm == 0)[0]
         pm_elecs = list(
             map(electrode_config.electrode_names.__getitem__, min_elecs_idx))
-
-        elecs_only = pace_maker.param_dist_normalized.loc[pm_elecs].drop(
-            columns=pace_maker.param_dist_normalized.columns[3:])
         pm_only_all_beats = pace_maker.param_dist_normalized.loc[pm_elecs]
+
+        # max_pm = pace_maker.param_dist_normalized.drop(
+        #     columns=["Electrode", "X", "Y"]).idxmax(axis=0)
+        # max_elecs = max_pm.values
+
+        max_pm = pace_maker.param_dist_normalized.drop(
+            columns=["Electrode", "X", "Y"]).max()
+        # print(pace_maker.param_dist_normalized.loc[
+        #     pace_maker.param_dist_normalized["Beat 25"] == max_pm[24], 
+        #     ["Electrode", "X", "Y", "Beat 25"]])
+
+        # print(pace_maker.param_dist_normalized.loc[
+        #     ["D11", "C11", "B10", "C10"], "Beat 25"])
 
         # "Size" of event, or length in beats the pacemaker is at some electrode
         event_length = 0
@@ -72,7 +82,6 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
         for num, beat in enumerate(pm_only_all_beats.columns[3:]):
             current_pm_idx = np.where(pm_only_all_beats[beat] == 0)
             num_pm = np.shape(current_pm_idx)[1]
-            
             # Decide between pacemaker electrodes in the event there are multiple
             # for a given beat
             if num_pm > 1:
@@ -83,6 +92,9 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
                 # When multiple minima exist, confirm their distances are under 
                 # the threshold.
                 current_pm = distance_calc(
+                    pace_maker.param_dist_normalized.loc[
+                        pace_maker.param_dist_normalized[beat] == max_pm[num], 
+                        ["Electrode", "X", "Y", beat]],
                     pm_only_all_beats[["Electrode", "X", "Y", beat]], 
                     temp_pm, thresh=thresh,
                     calc_mode="multi_min")
@@ -90,8 +102,7 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
                 if current_pm == None:
                     print(f"Widely dispersed pacemakers in {beat}.")
                     print("Consider re-evaluating your data.")
-                    # Stops event detection for problematic beat, 
-                    # proceeds to the next beat.
+                    # Stops event detection
                     break
                 pm_electrode.append(current_pm)
                 
@@ -108,6 +119,9 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
                         # Boolean check whether electrode distance exceeds
                         # threshold
                         check_thresh = distance_calc(
+                            pace_maker.param_dist_normalized.loc[
+                                pace_maker.param_dist_normalized[beat] == max_pm[num], 
+                                ["Electrode", "X", "Y", beat]],
                             pm_only_all_beats[["Electrode", "X", "Y", beat]], 
                             pm_old_new, thresh=thresh,
                             calc_mode="new_min")
@@ -140,6 +154,9 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
                         # Boolean check whether electrode distance exceeds
                         # threshold
                         check_thresh = distance_calc(
+                            pace_maker.param_dist_normalized.loc[
+                                pace_maker.param_dist_normalized[beat] == max_pm[num], 
+                                ["Electrode", "X", "Y", beat]],
                             pm_only_all_beats[["Electrode", "X", "Y", beat]], 
                             pm_old_new, thresh=thresh,
                             calc_mode="new_min")
@@ -173,13 +190,14 @@ def pm_translocations(analysisGUI, pace_maker, electrode_config):
         pace_maker.transloc_events = "No events."
 
 
-def distance_calc(pacemaker_only_df: DataFrame, pacemaker_elec: List, 
-thresh: int, calc_mode=""):
+def distance_calc(max_df: DataFrame, pacemaker_only_df: DataFrame, 
+pacemaker_elec: List, thresh: int, calc_mode=""):
 
     if calc_mode == "multi_min":
         temp_dists = []
         calc_df = pacemaker_only_df[["X", "Y"]]
         current_beat = pacemaker_only_df.columns[-1]
+        # print(pacemaker_elec)
         for fixed_elec in pacemaker_elec:
             pm_fixed = calc_df.loc[fixed_elec]
         
@@ -194,10 +212,32 @@ thresh: int, calc_mode=""):
         if all(dist < thresh for dist in temp_dists):
             return pacemaker_elec[0]
         else:
-            print(pacemaker_only_df)
-            print(f"{Fore.LIGHTRED_EX}Problem: pacemakers in {current_beat}" +
+            # Compare each minimum to each maximum time lag for given beat
+            # Calculate distance
+            max_elecs = max_df.index
+            temp_dists2 = {}
+            calc_max_df = max_df[["X", "Y"]]
+
+            for fixed_elec in pacemaker_elec:
+                pm_fixed = calc_df.loc[fixed_elec]
+        
+                for elec in max_elecs:
+                    x1 = calc_max_df.loc[elec, "X"]
+                    y1 = calc_max_df.loc[elec, "Y"]
+                    x2 = pm_fixed["X"]
+                    y2 = pm_fixed["Y"]
+                    dist = np.sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
+                    temp_dists2[fixed_elec + "_" + elec] = dist
+            pm_to_elec_max_dist = max(
+                temp_dists2.items(), key = lambda k : k[1])
+            sep = "_"
+            min_pm = pm_to_elec_max_dist[0].split(sep, 1)[0]
+
+            # print(f"Furthest PM-to-max time lag: {temp_dists2}")
+            # print(pacemaker_only_df.loc[pacemaker_elec])
+            print(f"{Fore.LIGHTRED_EX}ALERT: pacemakers in {current_beat}" +
                 f" are far apart.{Style.RESET_ALL}")
-            return None
+            return min_pm
 
     if calc_mode == "new_min":
         temp_dists = []
